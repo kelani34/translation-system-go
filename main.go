547a8691e9eaf5deb9ab800e8005c4b63e4570dd
kelani34 <kelanitaiwo34@gmail.com>
2024-06-16 02:17:44 +0100

    update: add translate func using NMT model

--- main.go
+++ main.go
@@ -16,6 +16,36 @@ func loadModel(modelPath string) (*tensorflow.SavedModel, error){
 }
 
 
+
+func translate(model *tensorflow.SavedModel, inputText string) (string, error) {
+	// Create a tensor from the input text
+	inputTensor, err := tensorflow.NewTensor(inputText)
+	if err != nil {
+		return "", fmt.Errorf("Error creating input tensor: %v", err)
+	}
+
+	// Run the model
+	output, err := model.Session.Run(
+		map[tensorflow.Output]*tensorflow.Tensor{
+			model.Graph.Operation("input_tensor_name").Output(0): inputTensor,
+		},
+		[]tensorflow.Output{
+			model.Graph.Operation("output_tensor_name").Output(0),
+		},
+		nil,
+	)
+	if err != nil {
+		return "", fmt.Errorf("Error running model: %v", err)
+	}
+
+	// Convert the output tensor to a string
+	translatedText := output[0].Value().(string)
+
+	return translatedText, nil
+}
+
+
+
 func main (){
 
 	modelPath := "" // our model path here
