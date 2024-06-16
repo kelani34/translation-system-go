a080b2110ebe34fe84665703592f4bcbe6d47ceb
kelani34 <kelanitaiwo34@gmail.com>
2024-06-16 02:14:21 +0100

    add: load model

--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
 )
 
+
+func loadModel(modelPath string) (*tensorflow.SavedModel, error){
+	model, err := tensorflow.LoadSavedModel(modelPath, []string{"serve"}, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error loading model: %v", err)
+	}
+	return model, nil
+}
+
+
 func main (){
+
+	modelPath := "" // our model path here
+	model, err := loadModel(modelPath)
+
+
 	inputText := "Hello world!"
 	
 	fmt.Println("Input text: ", inputText)
